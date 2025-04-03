8018684846221732edca649976db853b0cf14c25
GrupoCybersegurancaVirtus <cyberseguranca.virtus@gmail.com>
2025-04-03 08:31:21 -0300

    Ajustes nas remoção de duplicadas, implementação do módulo de Decision Tree
    Ajustes nas remoção de duplicadas, implementação do módulo de Decision Tree

--- main_DT.py
+++ main_DT.py
@@ -7,19 +7,20 @@ test_path = "G:/Meu Drive/Mestrado/VIRTUS/Código/Novo RuleXtract/data/rapid_bal
 
 # Parâmetros do modelo
 model_parameters = {
-    'n_estimators': 101,
-    'min_samples_leaf': 2,
+    'criterion': 'gini',
+    'splitter': 'best',
+    'max_depth': 10,
     'min_samples_split': 2,
-    'max_depth': None,
-    'max_features': 'sqrt',
+    'min_samples_leaf': 2,
+    'max_features': None,
     'random_state': 42
 }
 
 # Treinamento do modelo inicial
-classifier = RuleClassifier.new_classifier(train_path, test_path, model_parameters, algorithm_type='Random Forest')
+classifier = RuleClassifier.new_classifier(train_path, test_path, model_parameters, algorithm_type='Decision Tree')
 
 # Análise e remoção de regras
-classifier.execute_rule_analysis(test_path, remove_below_n_classifications=2)
+classifier.execute_rule_analysis(test_path, remove_below_n_classifications=-1)
 
 # Comparação dos resultados iniciais e finais
 classifier.compare_initial_final_results(test_path)
