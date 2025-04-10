bab0c6bbaef7a7310b8e91244b8151af0df80f64
GrupoCybersegurancaVirtus <89219720+caaiodiaas@users.noreply.github.com>
2025-04-10 08:24:11 -0300

    Aprimoramentos no rule_classifier
    Parâmetro "remove_duplciates" adicionado: True or False
    Métricas de interpretabilidade agora são exibidas ao final
    Alguns outros ajustes menores

--- main_RF.py
+++ main_RF.py
@@ -7,6 +7,14 @@ test_path = "data/rapid_balanceado_teste.csv"
 
 # Parâmetros do modelo
 model_parameters = {
+    'criterion': 'gini',
+    'bootstrap': True,
+    'oob_score': False,
+    'n_jobs': None,
+    'verbose': 0,
+    'warm_start': False,
+    'ccp_alpha': 0.0,
+    'max_samples': None,
     'n_estimators': 101,
     'min_samples_leaf': 2,
     'min_samples_split': 2,
@@ -19,7 +27,8 @@ model_parameters = {
 classifier = RuleClassifier.new_classifier(train_path, test_path, model_parameters, algorithm_type='Random Forest')
 
 # Análise e remoção de regras
-classifier.execute_rule_analysis(test_path, remove_below_n_classifications=-1)
+#(exact, strict, relaxed,...)
+classifier.execute_rule_analysis(test_path, remove_duplicates=True, remove_below_n_classifications=-1)
 
 # Comparação dos resultados iniciais e finais
 classifier.compare_initial_final_results(test_path)
