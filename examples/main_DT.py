8375d0cb95bb295dee4a69fdc8453c8890725e87
Lucas Lopes <j.lucasvinicius03@gmail.com>
2025-04-14 11:33:39 -0300

    Reorganização da estrutura do projeto e configuração do pyproject.toml

--- examples/main_DT.py
+++ examples/main_DT.py
@@ -1,4 +1,4 @@
-from rule_classifier import RuleClassifier
+from pyruleanalyzer import RuleClassifier
 
 # ************************ EXECUÇÃO ************************
 
@@ -8,26 +8,22 @@ test_path = "data/rapid_balanceado_teste.csv"
 # Parâmetros do modelo
 model_parameters = {
     'criterion': 'gini',
-    'bootstrap': True,
-    'oob_score': False,
-    'n_jobs': None,
-    'verbose': 0,
-    'warm_start': False,
+    'splitter': 'best',
+    'max_leaf_nodes': None,
+    'min_impurity_decrease': 0.0,
+    'class_weight': None,
     'ccp_alpha': 0.0,
-    'max_samples': None,
-    'n_estimators': 101,
-    'min_samples_leaf': 2,
+    'max_depth': 10,
     'min_samples_split': 2,
-    'max_depth': None, 
-    'max_features': 'sqrt',
+    'min_samples_leaf': 2,
+    'max_features': None,
     'random_state': 42
 }
 
 # Treinamento do modelo inicial
-classifier = RuleClassifier.new_classifier(train_path, test_path, model_parameters, algorithm_type='Random Forest')
+classifier = RuleClassifier.new_classifier(train_path, test_path, model_parameters, algorithm_type='Decision Tree')
 
 # Análise e remoção de regras
-#(exact, strict, relaxed,...)
 classifier.execute_rule_analysis(test_path, remove_duplicates=True, remove_below_n_classifications=-1)
 
 # Comparação dos resultados iniciais e finais
