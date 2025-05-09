1441f58e75774dad39bc1bb7b01d9c2a4576304d
Caio Dias <caiodg2001@gmail.com>
2025-05-08 22:00:50 -0300

    Versão 1.0
    Mudanças na detecção de duplicadas
    Mudanças na exibição dos resultados
    Diversos aprimoramentos no código

--- examples/main_RF.py
+++ examples/main_RF.py
@@ -1,11 +1,11 @@
-from rule_classifier import RuleClassifier
+from pyruleanalyzer.rule_classifier import RuleClassifier
 
 # ************************ EXECUÇÃO ************************
 
-train_path = "data/rapid_balanceado_treinamento.csv"
-test_path = "data/rapid_balanceado_teste.csv"
+train_path = "examples/data/rapid_balanceado_treinamento.csv"
+test_path = "examples/data/rapid_balanceado_teste.csv"
 
-# Parâmetros do modelo
+# Model parameters
 model_parameters = {
     'criterion': 'gini',
     'bootstrap': True,
@@ -15,20 +15,21 @@ model_parameters = {
     'warm_start': False,
     'ccp_alpha': 0.0,
     'max_samples': None,
-    'n_estimators': 101,
+    'n_estimators': 10,
     'min_samples_leaf': 2,
     'min_samples_split': 2,
-    'max_depth': None, 
+    'max_depth': None,
     'max_features': 'sqrt',
     'random_state': 42
 }
 
-# Treinamento do modelo inicial
+# Generating the initial rule based model
 classifier = RuleClassifier.new_classifier(train_path, test_path, model_parameters, algorithm_type='Random Forest')
 
-# Análise e remoção de regras
-#(exact, strict, relaxed,...)
-classifier.execute_rule_analysis(test_path, remove_duplicates=True, remove_below_n_classifications=-1)
+# Executing the rule analysis method
+# remove_duplicates = "soft" (in the same tree, probably does not affect the final metrics), "hard" (between trees, may affect the final metrics), "custom" (custom function to remove duplicates) or "none" (no removal)
+# remove_below_n_classifications = -1 (no removal), 0 (removal of rules with 0 classifications), or any other integer (removal of rules with equal or less than n classifications)
+classifier.execute_rule_analysis(test_path, remove_duplicates="soft", remove_below_n_classifications=-1)
 
-# Comparação dos resultados iniciais e finais
+# Comparing initial and final results
 classifier.compare_initial_final_results(test_path)
