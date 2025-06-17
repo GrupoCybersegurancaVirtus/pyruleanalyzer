90058a0704d52b8c1984c87f1bec87c062554314
Caio Dias <caiodg2001@gmail.com>
2025-06-17 08:46:13 -0300

    Adjustments on the "hard" method
    Adjustments on the "hard" method
    Minor fixes

--- examples/main_RF.py
+++ examples/main_RF.py
@@ -2,14 +2,14 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from pyruleanalyzer.rule_classifier import RuleClassifier
 
-train_path = "examples/data/rapid_balanceado_treinamento.csv"
-test_path = "examples/data/rapid_balanceado_teste.csv"
+# train_path = "examples/data/rapid_balanceado_treinamento.csv"
+# test_path = "examples/data/rapid_balanceado_teste.csv"
 
 # train_path = "examples/data/mushrooms_encoded_train.csv"
 # test_path = "examples/data/mushrooms_encoded_test.csv"
 
-# train_path = "examples/data/iris_train.csv"
-# test_path = "examples/data/iris_test.csv"
+train_path = "examples/data/iris_train.csv"
+test_path = "examples/data/iris_test.csv"
 
 # Model parameters
 model_parameters = {
@@ -31,7 +31,7 @@ classifier = RuleClassifier.new_classifier(train_path, test_path, model_paramete
 # Executing the rule analysis method
 # remove_duplicates = "soft" (in the same tree, probably does not affect the final metrics), "hard" (between trees, may affect the final metrics), "custom" (custom function to remove duplicates) or "none" (no removal)
 # remove_below_n_classifications = -1 (no removal), 0 (removal of rules with 0 classifications), or any other integer (removal of rules with equal or less than n classifications)
-classifier.execute_rule_analysis(test_path, remove_duplicates="soft", remove_below_n_classifications=-1)
+classifier.execute_rule_analysis(test_path, remove_duplicates="hard", remove_below_n_classifications=1)
 
 # Comparing initial and final results
 classifier.compare_initial_final_results(test_path)
